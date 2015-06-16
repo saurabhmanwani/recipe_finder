5954410c4c9c283387406853481c354bd483b293
Saurabh Manwani <saurabh.manwani@optus.com.au>
2015-06-16 21:05:35 +1000

    1st in branch1

--- RunRecipeFinder.sh
+++ RunRecipeFinder.sh
@@ -4,5 +4,5 @@ LIB=./jars/
  
 CP=.:${LIB}json-20140107.jar:${LIB}junit-4.11.jar
  
-javac -cp "${CP}" com/pactera/recipefinder/RecipeFinder.java
+javac -cp "${CP}" com/pactera/recipefinder/RecipeFinder.jav
 java -cp "${CP}"  com.pactera.recipefinder.RecipeFinder
